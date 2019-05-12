602641a9601cf6d817d9147d6912897e97e839a3
Thomas Cacciatore <cacciato.thomas@gmail.com>
2019-05-12 10:49:39 +0200

    Refactor article cell UI.

--- lib/views/ArticleCell.dart
+++ lib/views/ArticleCell.dart
@@ -5,29 +5,34 @@ import 'package:cached_network_image/cached_network_image.dart';
 class ArticleCell extends StatelessWidget {
   final Article article;
 
+  // Default image in case the article image is missing.
   static const String DEFAULT_IMAGE = "https://news.delta.com/sites/all/themes/delta_news_hub_omega/images/Apple_News_Icon.png";
 
+  // Widget constructor.
   ArticleCell(this.article);
 
   @override
   Widget build(BuildContext context) {
 
+    // Extract www.websiteName.com from the complete url.
     final shorterUrl = this.article.url.split('/')[2];
 
     return Container(
-      padding: EdgeInsets.all(16),
+
+      // Display the shorter url.
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(bottom: 8),
+            padding: EdgeInsets.all(8),
             child: Text(shorterUrl,
                 textAlign: TextAlign.left,
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.w200)),
           ),
 
+          // Display the title.
           Padding(
-            padding: EdgeInsets.only(bottom: 8),
+            padding: EdgeInsets.all(8),
             child: Text(
               this.article.title,
               textAlign: TextAlign.left,
@@ -35,8 +40,8 @@ class ArticleCell extends StatelessWidget {
             ),
           ),
 
-          Padding(
-            padding: EdgeInsets.only(bottom: 8),
+          // Display the image.
+          Container(
             child: CachedNetworkImage(
               imageUrl: this.article.urlToImage != null ? this.article.urlToImage : DEFAULT_IMAGE,
               placeholder: (context, url) => new CircularProgressIndicator(),
